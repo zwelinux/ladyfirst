ebd357cb3e4795ae024010e3bee5740c373b7981
zwelinux <zwelinux@gmail.com>
2025-01-17 14:25:47 +0700

    models for product only

--- backend/ladyfirst/models.py
+++ backend/ladyfirst/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    seller = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    brand_name = models.CharField(max_length=100)
+    size = models.CharField(max_length=50, blank=True, null=True)
+    condition = models.CharField(max_length=50)
+    original_price = models.DecimalField(max_digits=10, decimal_places=2)
+    second_hand_price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    image_urls = models.JSONField()
+    authenticity_docs = models.JSONField(blank=True, null=True)
+    status = models.CharField(max_length=10, choices=[('active', 'Active'), ('sold', 'Sold')], default='active')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
