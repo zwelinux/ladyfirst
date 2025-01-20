a0f84f221bc66ba37324b4877331b6388b5919c4
zwelinux <zwelinux@gmail.com>
2025-01-20 16:16:18 +0700

    20 jan updates

--- backend/ladyfirst/admin.py
+++ backend/ladyfirst/admin.py
@@ -3,4 +3,10 @@ from .models import *
 
 admin.site.register(Product)
 
+admin.site.register(User)
+admin.site.register(Transaction)
+admin.site.register(SharedLink)
+admin.site.register(Favorite)
+admin.site.register(Admin)
+
 admin.site.site_header = 'LadyFirst.Me'
